9225dc926824c92d1ffe7f7fb3c5d36cc1d52a40
Brennon Cupp <cuppb@student.swosu.edu>
2015-10-07 09:49:06 -0500

    made an outline

--- src/MathGame.java
+++ src/MathGame.java
@@ -3,6 +3,26 @@ public class MathGame {
 	public static void main(String[] args) {
 		System.out.println("Hello");
 
+		//outline for addition problem
+		
+		/* Round 1
+		 * Generate two random numbers
+		 * ask the user to add the two numbers
+		 * read in their response
+		 * check if correct
+		 *  	If Correct
+		 *  		say correct
+		 *  		give them points
+		 *  		make the questions harder
+		 *  	If not Correct
+		 *  		say incorrect
+		 *  		do not give them points
+		 *  		make the questions easier
+		 * Round 2
+		 * Round 3
+		 * Round 4
+		 */
+	
 	}
 
 }
